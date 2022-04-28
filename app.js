62f7ee099297896bc82ac1fb5ab163037eecb156
David <Rigby335@hotmail.com>
2022-04-28 16:07:42 -0700

    added eventlisteners and images

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // import functions and grab DOM elements
 const treeElem = document.getElementById('tree-select');
 const groundElem = document.getElementById('ground-select');
+const animalElem = document.getElementById('animal-select');
 
 const treeDiv = document.getElementById('tree');
 const groundDiv = document.getElementById('ground');
-
+const animalDiv = document.getElementById('animal');
 
 // let state
 let treeCount = 0;
+
 // set event listeners
 treeElem.addEventListener('change', () => {
     console.log('show', treeElem.value);
@@ -16,8 +18,15 @@ treeElem.addEventListener('change', () => {
 
 groundElem.addEventListener('change', () => {
     console.log('show', groundElem.value);
+    groundDiv.style.backgroundImage = `url('./assets/${groundElem.value}.jpg')`;
+});
+
+animalElem.addEventListener('change', () => {
+    console.log('show', animalElem.value);
+    animalDiv.style.backgroundImage = `url('./assets/${animalElem.value}.jpg')`;
 });
   // get user input
+
   // use user input to update state 
   // update DOM to reflect the new state
   // e.target.value
