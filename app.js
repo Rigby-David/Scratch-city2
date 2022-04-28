0fd589648f6a6788df2acb40a549b16fa0736519
David <Rigby335@hotmail.com>
2022-04-28 14:44:32 -0700

    added view and grabbed elements

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // import functions and grab DOM elements
+const treeElem = document.getElementById('tree-select');
+
+const treeDiv = document.getElementById('tree');
 
 // let state
+let treeCount = 0;
+// set event listeners
+treeElem.addEventListener('change', () => {
 
-// set event listeners 
+}); 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
