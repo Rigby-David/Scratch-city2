89d61e63a32353ad2408759580146b7df69a5225
David <Rigby335@hotmail.com>
2022-04-28 15:45:33 -0700

    CSS sizing work and added more images

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // import functions and grab DOM elements
 const treeElem = document.getElementById('tree-select');
+const groundElem = document.getElementById('ground-select');
 
 const treeDiv = document.getElementById('tree');
+const groundDiv = document.getElementById('ground');
+
 
 // let state
 let treeCount = 0;
@@ -9,7 +12,12 @@ let treeCount = 0;
 treeElem.addEventListener('change', () => {
     console.log('show', treeElem.value);
     treeDiv.style.backgroundImage = `url('./assets/${treeElem.value}.jpg')`;
-}); 
+});
+
+groundElem.addEventListener('change', () => {
+    console.log('show', groundElem.value);
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+  // e.target.value
